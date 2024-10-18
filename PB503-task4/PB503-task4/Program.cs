7a27afbfffa3b571bec5ec34568c58f8d618e4b4
Zumrud005 <mustafazuzu05@gmail.com>
2024-10-18 23:08:10 +0400

    added tasks and flowchart png

---
+++ PB503-task4/PB503-task4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PB503_task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Sadə və ya mürəkkəb ədədin tapılması
+            //Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapan alqoritm.
+            //Console.WriteLine("Ededi daxil edin:");
+            //int number=Convert.ToInt32(Console.ReadLine());
+            //bool check = true;
+            //if (number == 1)
+            //{
+            //    Console.WriteLine("Bu eded ne sade ne de murekkebdir.");
+            //}
+            //else
+            //{
+            //    for (int i = 2; i < number; i++)
+            //    {
+            //        if (number % i == 0)
+            //        {
+            //            check = false;
+            //            break;
+            //        }
+            //    }
+            //    if (check)
+            //    {
+            //        Console.WriteLine($"{number} ededi sadedir");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{number} ededi murekkebdir");
+            //    }
+            //}
+
+            #endregion
+
+            #region Ədədin rəqəmlərinin sayının tapılması
+            //Verilmiş n tam ədədinin neçə mərtəbəli olduğunu tapın. Məs: 23452, output: 5 , Məs: 456, output: 3
+            //Console.WriteLine("Ededi daxil edin");
+            //int number=Convert.ToInt32(Console.ReadLine());
+            //int reqemSayi = 0;
+            //do
+            //{
+            //    number = number / 10;
+            //    reqemSayi++;
+            //}
+            //while (number != 0);
+            //Console.WriteLine($"Bu ededin reqemlerinin sayi={reqemSayi}");
+
+           
+
+            #endregion
+
+        }
+    }
+}
